ece8b00703f0d9ee1bf7097d2d20327842bbb266
BASTIEN VITOUR <bastien.vitour@edu.esiee-it.fr>
2023-10-17 15:17:05 +0200

    add search function

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -9,11 +9,22 @@ import {api_key} from "./api_key";
 export default function Movies() {
 
     const [movies, setMovies] = useState([])
+    const [query, setQuery] = useState("")
     const [page, setPage] = useState(1)
     const location = useLocation()
+    const baseUrl = "https://api.themoviedb.org/3"
 
     const getMovies = async (page) => {
-        await axios.get(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${page}&api_key=${api_key}`)
+        await axios.get(`${baseUrl}/movie/top_rated?language=en-US&page=${page}&api_key=${api_key}`)
+        .then((res) => {
+            setMovies(res.data.results)
+        })
+        .catch((error) => console.error(error))
+    }
+
+    const searchMovies = async (e) => {
+        e.preventDefault()
+        await axios.get(`${baseUrl}/search/movie?query=${query}?&include_adult=false&language=en-US&page=1&api_key=${api_key}`)
         .then((res) => {
             setMovies(res.data.results)
         })
@@ -31,12 +42,12 @@ export default function Movies() {
     }, [page])
 
     useEffect(() => {
-        const urlParams = new URLSearchParams(location.search)
-        if(urlParams.has("page")) {
-            setPage(urlParams.get('page'))
-            //getMovies(urlParams.get('page'))
-        }
-        getMovies(page)
+        // const urlParams = new URLSearchParams(location.search)
+        // if(urlParams.has("page")) {
+        //     setPage(urlParams.get('page'))
+        //     //getMovies(urlParams.get('page'))
+        // }
+        // getMovies(page)
         return () => {}
     }, [])
 
@@ -51,6 +62,10 @@ export default function Movies() {
                     <Icon.ArrowRightSquareFill fontSize={35} className="arrow" onClick={() => setPage(parseInt(page)+1)} />
                 {/* </Link> */}
             </div>
+            <form onSubmit={searchMovies}>
+                <input onChange={(e) => setQuery(e.target.value)} placeholder="Search Movie" />
+                <button><Icon.Search />Search</button>
+            </form>
             <ul>
                 {movies.map((movie) => {
                     return(
